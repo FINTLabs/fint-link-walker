6cc5440358b9efd1a060ab32321b66622ae74f42
jarlehansen <jarle@jarlehansen.net>
2017-12-04 20:54:17 +0100

    added springfox-loader

--- src/main/java/no/fint/Application.java
+++ src/main/java/no/fint/Application.java
@@ -1,8 +1,10 @@
 package no.fint;
 
+import com.github.springfox.loader.EnableSpringfox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableSpringfox
 @SpringBootApplication
 public class Application {
 
