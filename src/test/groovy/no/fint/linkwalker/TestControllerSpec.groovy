e4091881d4982721394375a90a08eb7dc8c95d18
jarlehansen <jarle@jarlehansen.net>
2018-02-04 16:57:58 +0100

    fixed TestController tests

--- src/test/groovy/no/fint/linkwalker/TestControllerSpec.groovy
+++ src/test/groovy/no/fint/linkwalker/TestControllerSpec.groovy
@@ -1,22 +1,41 @@
 package no.fint.linkwalker
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import no.fint.linkwalker.dto.TestCase
+import no.fint.linkwalker.dto.TestRequest
 import no.fint.test.utils.MockMvcSpecification
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 
 class TestControllerSpec extends MockMvcSpecification {
     private MockMvc mockMvc
+    private TestScheduler testScheduler
     private TestController controller
 
     void setup() {
-        controller = new TestController()
+        testScheduler = Mock(TestScheduler)
+        controller = new TestController(testScheduler: testScheduler)
         mockMvc = standaloneSetup(controller)
     }
 
-    def "Start test"() {
+    def "Start test without including TestRequest"() {
         when:
         def response = mockMvc.perform(post('/tests'))
 
         then:
-        response.andExpect(status().isOk())
+        response.andExpect(status().isBadRequest())
+    }
+
+    def "Start test with TestRequest"() {
+        when:
+        def request = new TestRequest(endpoint: '/test')
+        def body = new ObjectMapper().writeValueAsString(request)
+        def response = mockMvc.perform(post('/tests')
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(body))
+
+        then:
+        1 * testScheduler.scheduleTest(_ as String) >> new TestCase(UUID.randomUUID(), '/test')
+        response.andExpect(status().isCreated())
     }
 }
