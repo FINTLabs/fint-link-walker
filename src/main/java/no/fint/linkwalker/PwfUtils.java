efe0d65a876ad46a51701a5a4267b5c0983188e1
Asgeir Nilsen <asgeir@fintlabs.no>
2019-08-07 15:56:12 +0200

    Fix handling if PWF locations.

--- src/main/java/no/fint/linkwalker/PwfUtils.java
+++ src/main/java/no/fint/linkwalker/PwfUtils.java
@@ -1,10 +1,12 @@
 package no.fint.linkwalker;
 
+import org.apache.commons.lang3.StringUtils;
+
 public enum PwfUtils {
     ;
 
-    public static boolean isPwf(String baseUrl) {
-        return baseUrl.equals(Constants.PWF_BASE_URL);
+    public static boolean isPwf(String location) {
+        return StringUtils.startsWith(location, Constants.PWF_BASE_URL);
     }
 
 }
