6f7eac99660969ad0b67aaa48c38e95a9a43e337
Hknots <hknots99@gmail.com>
2024-02-13 23:03:16 +0100

    simplify class & rename methods

--- src/main/java/no/fint/linkwalker/oauth2/ReactiveOauth2Factory.java
+++ src/main/java/no/fint/linkwalker/oauth2/ReactiveOauth2Factory.java
@@ -4,26 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.portal.model.client.Client;
 import no.fint.portal.model.client.ClientService;
-import no.fintlabs.core.resource.server.security.converter.CorePrincipalConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
@@ -31,42 +20,16 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ReactiveOauth2Factory {
 
-    private final Map<String, String> registrationIdMap = new HashMap<>();
-    private final CorePrincipalConverter converter = new CorePrincipalConverter();
-    private final JwtDecoder jwtDecoder;
+    private final Map<String, AccessToken> accessTokenMap = new HashMap<>();
     private final ClientService clientService;
-    private final ServerOAuth2AuthorizedClientRepository authorizedClientRepository;
     private final WebClient webClient = WebClient.create();
 
-    @Bean("registrationIdMap")
-    Map<String, String> registrationIdMap() {
-        return registrationIdMap;
-    }
-
-    // TODO: Handle bug scenario
-    // A bug may occur if a client's password is reset after it's been registered by the service.
-    public void registerNewClient(String clientName, String organisationName, ServerWebExchange serverWebExchange) {
-        if (registrationIdMap.containsKey(clientName)) {
-            return;
+    public String getBearerToken(String clientName, String organisationName) {
+        if (!accessTokenMap.containsKey(clientName) || accessTokenMap.get(clientName).getExpiresIn() < 60) {
+            Client client = clientService.getClient(clientName, organisationName).orElseThrow(SecurityException::new);
+            createNewAccessToken(client).subscribe(accessToken -> accessTokenMap.put(clientName, accessToken));
         }
-
-        String registrationId = UUID.randomUUID().toString();
-        registrationIdMap.put(clientName, registrationId);
-
-        Client client = clientService.getClient(clientName, organisationName).orElseThrow(SecurityException::new);
-        ClientRegistration clientRegistration = createClientRegistration(client, registrationId);
-        createOuath2AuthorizedClient(clientRegistration, client, serverWebExchange);
-    }
-
-    private ClientRegistration createClientRegistration(Client client, String registrationId) {
-        return ClientRegistration.withRegistrationId(registrationId)
-                .clientName(client.getName())
-                .clientId(client.getClientId())
-                .clientSecret(clientService.getClientSecret(client))
-                .tokenUri("https://idp.felleskomponent.no/nidp/oauth/nam/token")
-                .authorizationGrantType(AuthorizationGrantType.PASSWORD)
-                .scope("fint-client")
-                .build();
+        return accessTokenMap.get(clientName).getAccessToken();
     }
 
     private String resetPassword(Client client) {
@@ -75,7 +38,7 @@ public class ReactiveOauth2Factory {
         return password;
     }
 
-    private Mono<AccessToken> getAccessToken(Client client) {
+    private Mono<AccessToken> createNewAccessToken(Client client) {
         return webClient.post()
                 .uri("https://idp.felleskomponent.no/nidp/oauth/nam/token")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
@@ -88,32 +51,6 @@ public class ReactiveOauth2Factory {
                         .with("scope", "fint-client"))
                 .retrieve()
                 .bodyToMono(AccessToken.class);
-    }
-
-    private void createOuath2AuthorizedClient(ClientRegistration clientRegistration, Client client, ServerWebExchange serverWebExchange) {
-        getAccessToken(client).subscribe(fintAccessToken -> {
-            Jwt jwt = jwtDecoder.decode("bearer " + fintAccessToken.getAccessToken());
-            FintToken fintToken = new FintToken(jwt);
-
-            OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(
-                    OAuth2AccessToken.TokenType.BEARER,
-                    fintAccessToken.getAccessToken(),
-                    fintToken.getIssuedAtTime(),
-                    fintToken.getExpirationTime()
-            );
-
-            OAuth2AuthorizedClient oAuth2AuthorizedClient = new OAuth2AuthorizedClient(
-                    clientRegistration,
-                    client.getName(),
-                    oAuth2AccessToken
-            );
-
-            Objects.requireNonNull(converter.convert(jwt))
-                    .doOnError(throwable -> log.error(throwable.getMessage()))
-                    .subscribe(authentication -> {
-                        authorizedClientRepository.saveAuthorizedClient(oAuth2AuthorizedClient, authentication, serverWebExchange).subscribe();
-                    });
-        });
     }
 
 }
