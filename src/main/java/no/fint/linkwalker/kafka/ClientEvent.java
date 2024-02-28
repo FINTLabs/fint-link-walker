7e60e0bcad9fd8f75505412484df431087ad7852
Hknots <hknots99@gmail.com>
2024-02-28 13:56:27 +0100

    add cleintEventRequestProducerService

---
+++ src/main/java/no/fint/linkwalker/kafka/ClientEvent.java
@@ -0,0 +1,16 @@
+package no.fint.linkwalker.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@AllArgsConstructor
+@SuperBuilder
+public class ClientEvent extends FintCustomerObjectEvent<Client> {
+
+
+    public ClientEvent(Client object, String orgId, FintCustomerObjectEvent.Operation operation) {
+        super(object, orgId, operation, null);
+    }
+}
