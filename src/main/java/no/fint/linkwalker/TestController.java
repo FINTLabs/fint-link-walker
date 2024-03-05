5a1684772636f0dfab441c4fbfe46f8d25d4023f
Hknots <hknots99@gmail.com>
2024-03-05 09:45:31 +0100

    adjust requestMapping

--- src/main/java/no/fint/linkwalker/TestController.java
+++ src/main/java/no/fint/linkwalker/TestController.java
@@ -29,7 +29,7 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @CrossOrigin
-@RequestMapping("/api/tests/{organisation}/links")
+@RequestMapping("/{organisation}/links")
 public class TestController {
 
     @Autowired
