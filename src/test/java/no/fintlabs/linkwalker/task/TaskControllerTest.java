1502fa68acd2fbf2fc24c416cf74a33497dec38d
Hknots <hknots99@gmail.com>
2024-04-04 12:42:47 +0200

    add requestValidation tests

--- src/test/java/no/fintlabs/linkwalker/task/TaskControllerTest.java
+++ src/test/java/no/fintlabs/linkwalker/task/TaskControllerTest.java
@@ -1,2 +1,57 @@
-package no.fintlabs.linkwalker.task;public class TaskControllerTest {
+package no.fintlabs.linkwalker.task;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TaskControllerTest {
+
+    private TaskController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new TaskController();
+    }
+
+    @Test
+    void testRequestIsValid() {
+        Task task = Task.builder()
+                .url("https://example.com")
+                .clientName("clientName")
+                .org("org")
+                .build();
+
+        assertFalse(controller.requestNotValid(task, "Bearer Token, nice to meet you."));
+    }
+
+    @Test
+    void testRequestIsInvalidDueToMissingURL() {
+        Task task = Task.builder()
+                .clientName("clientName")
+                .org("org")
+                .build();
+
+        assertTrue(controller.requestNotValid(task, "Hi im bearer token."));
+    }
+
+    @Test
+    void testRequestIsInvalidDueToMissingClientNameAndOrgWithoutAuthHeader() {
+        Task task = Task.builder()
+                .url("https://example.com")
+                .build();
+
+        assertTrue(controller.requestNotValid(task, null));
+    }
+
+    @Test
+    void testRequestIsValidWithAuthHeaderOnly() {
+        Task task = Task.builder()
+                .url("https://example.com")
+                .build();
+
+        assertFalse(controller.requestNotValid(task, "Am I the bearer of token? Yes, I am."));
+    }
+
 }
