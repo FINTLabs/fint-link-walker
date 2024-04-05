99525e5eb3462dbce4604979d62d98f4d1295639
Hknots <hknots99@gmail.com>
2024-04-05 13:51:58 +0200

    add request body models

---
+++ src/main/java/no/fintlabs/linkwalker/request/model/Link.java
@@ -0,0 +1,4 @@
+package no.fintlabs.linkwalker.request.model;
+
+public record Link(String href) {
+}
---
+++ src/main/java/no/fintlabs/linkwalker/request/model/Request.java
@@ -0,0 +1,4 @@
+package no.fintlabs.linkwalker.request.model;
+
+public record Request(Embedded _embedded) {
+}
