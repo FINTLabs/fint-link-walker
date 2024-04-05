7a211877210ed6d765b0682d08125dc3a82dbb00
Hknots <hknots99@gmail.com>
2024-04-05 13:53:54 +0200

    add base structure of LinkWalker

--- src/main/java/no/fintlabs/linkwalker/LinkWalker.java
+++ src/main/java/no/fintlabs/linkwalker/LinkWalker.java
@@ -1,14 +1,28 @@
 package no.fintlabs.linkwalker;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.linkwalker.request.RequestService;
 import no.fintlabs.linkwalker.task.Task;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Async
 @Component
+@Slf4j
+@RequiredArgsConstructor
 public class LinkWalker {
 
+    private final RequestService requestService;
+
     public void processTask(Task task) {
+        if (task.getToken() == null) {
+            // Customer-Object-Gateway to fetch client credentials & Set token
+        }
+
+        requestService.getRequest(task.getUrl(), task.getToken()).subscribe(requestResponse -> {
+            log.info(requestResponse.toString());
+        });
 
     }
 
