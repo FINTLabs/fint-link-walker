397d95b7da5bb7a2b5b5022b7bd0a8663535c62f
Hknots <hknots99@gmail.com>
2024-04-08 09:02:31 +0200

    add unlimited buffer size

--- src/main/java/no/fintlabs/linkwalker/config/WebClientConfig.java
+++ src/main/java/no/fintlabs/linkwalker/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package no.fintlabs.linkwalker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -10,7 +11,17 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
+                .exchangeStrategies(unlimitedBufferSize())
                 .build();
+    }
+
+    private ExchangeStrategies unlimitedBufferSize() {
+        return ExchangeStrategies.builder()
+                .codecs(configurer -> configurer
+                        .defaultCodecs()
+                        .maxInMemorySize(-1))
+                .build();
+
     }
 
 }
