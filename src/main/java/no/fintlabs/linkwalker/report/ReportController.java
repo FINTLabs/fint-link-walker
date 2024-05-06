bfca845cdfdeab4d76bae1b8ecb12b7a62548c57
Hknots <hknots99@gmail.com>
2024-05-06 13:39:22 +0200

    use ReportService & add excel download endpoint

--- src/main/java/no/fintlabs/linkwalker/report/ReportController.java
+++ src/main/java/no/fintlabs/linkwalker/report/ReportController.java
@@ -2,7 +2,11 @@ package no.fintlabs.linkwalker.report;
 
 import lombok.RequiredArgsConstructor;
 import no.fintlabs.linkwalker.report.model.Report;
+import no.fintlabs.linkwalker.task.Task;
 import no.fintlabs.linkwalker.task.TaskCache;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/report/{organization}")
@@ -17,6 +22,7 @@ import java.util.Collection;
 public class ReportController {
 
     private final TaskCache taskCache;
+    private final ReportService reportService;
 
     @GetMapping
     public ResponseEntity<Collection<Report>> getReports(@PathVariable String organization) {
@@ -25,9 +31,26 @@ public class ReportController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Report> getReport(@PathVariable String organization, @PathVariable String id) {
-        return taskCache.exists(organization, id)
-                ? ResponseEntity.ok(Report.ofTask(taskCache.get(organization, id).get()))
-                : ResponseEntity.notFound().build();
+        return taskCache.get(organization, id)
+                .map(task -> ResponseEntity.ok(Report.ofTask(task)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{id}/download")
+    public ResponseEntity<byte[]> downloadReport(@PathVariable String organization, @PathVariable String id) {
+        Optional<Task> optionalTask = taskCache.get(organization, id);
+
+        if (optionalTask.isPresent()) {
+            byte[] spreadsheet = reportService.createSpreadSheet(optionalTask.get());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", "relasjonstest.xlsx");
+
+            return new ResponseEntity<>(spreadsheet, headers, HttpStatus.OK);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
