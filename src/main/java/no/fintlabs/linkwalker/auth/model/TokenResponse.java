02cde894b0dd0aa8d6833d7e0520a1576e51d679
Hknots <hknots99@gmail.com>
2024-11-11 10:43:53 +0100

    add jsonproperties for fields

--- src/main/java/no/fintlabs/linkwalker/auth/model/TokenResponse.java
+++ src/main/java/no/fintlabs/linkwalker/auth/model/TokenResponse.java
@@ -1,9 +1,11 @@
 package no.fintlabs.linkwalker.auth.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record TokenResponse (
-        String accesToken,
-        String tokenType,
-        int expiresIn,
+        @JsonProperty("access_token") String accesToken,
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("expires_in") int expiresIn,
         String acr,
         String scope
 ) {
