43b55c23cdb4a90699f322a19bc747a5c25bf478
Hknots <hknots99@gmail.com>
2025-05-13 23:11:23 +0200

    feat: add TaskController for managing task creation

---
+++ src/main/kotlin/no/fintlabs/linkwalker/task/TaskController.kt
@@ -0,0 +1,71 @@
+package no.fintlabs.linkwalker.task
+
+import no.fintlabs.linkwalker.task.model.TaskRequest
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.lang.String
+
+@RestController
+@RequestMapping("/tasks/{orgId}")
+class TaskController(
+    private val taskService: TaskService
+) {
+
+    @PostMapping
+    fun createTask(
+        @PathVariable orgId: String,
+        @RequestBody taskRequest: TaskRequest,
+        @RequestHeader(value = "Authorization", required = false) authHeader: String?
+    ) =
+
+//    @GetMapping
+//    fun getTasks(@PathVariable organization: String?): ResponseEntity<MutableCollection<Task?>?> {
+//        return ResponseEntity.ok<T?>(taskService.getTasks(organization))
+//    }
+//
+//    @GetMapping("/{id}")
+//    fun getTask(@PathVariable organization: String?, @PathVariable id: String?): ResponseEntity<Task?> {
+//        return taskService.getTask(organization, id)
+//            .map(ResponseEntity::ok)
+//            .orElseGet({ ResponseEntity.notFound().build<T?>() })
+//    }
+//
+//    @GetMapping("/{id}/download")
+//    fun downloadRelationErrors(
+//        @PathVariable organization: String?,
+//        @PathVariable id: String?
+//    ): ResponseEntity<ByteArray?> {
+//        val optionalTask: Optional<Task> = taskService.getTask(organization, id)
+//
+//        if (optionalTask.isPresent()) {
+//            val task: Task = optionalTask.get()
+//            val spreadsheet: ByteArray? = spreadsheetService.createSpreadSheet(task.getEntryReports())
+//
+//            val headers = HttpHeaders()
+//            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM)
+//            headers.setContentDispositionFormData("attachment", String.format("relasjonstest-%s.xlsx", task.getTime()))
+//
+//            return ResponseEntity<ByteArray?>(spreadsheet, headers, HttpStatus.OK)
+//        }
+//
+//        return ResponseEntity.notFound().build<ByteArray?>()
+//    }
+//
+//    @PutMapping("/{id}")
+//    fun clearTask(@PathVariable organization: kotlin.String?, @PathVariable id: kotlin.String?): ResponseEntity<*> {
+//        taskService.clearTask(organization, id)
+//        return ResponseEntity.ok().build<Any?>()
+//    }
+//
+//
+//    @PutMapping
+//    fun clearTasks(@PathVariable organization: kotlin.String?): ResponseEntity<*> {
+//        taskService.clearCache(organization)
+//        return ResponseEntity.ok().build<Any?>()
+//    }
+
+}
