24226c3c30691b9b1d92b0ab6b96978956d8a63b
Hknots <hknots99@gmail.com>
2025-05-13 23:08:58 +0200

    refactor: update getBearerToken to handle nullable authHeader

--- src/main/kotlin/no/fintlabs/linkwalker/auth/AuthService.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/auth/AuthService.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service
 @Service
 class AuthService {
 
-    fun getBearerToken(authHeader: String, client: String?): String? =
-        authHeader.removePrefix("Bearer ")
+    fun getBearerToken(authHeader: String?, client: String?): String? =
+        authHeader?.removePrefix("Bearer ")
 
 }
