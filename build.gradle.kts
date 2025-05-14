b41eb574d7e07c45e338f5b4a9e1a502198baa97
Hknots <hknots99@gmail.com>
2025-05-14 13:57:46 +0200

    feat: add Caffeine cache dependency for improved performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
