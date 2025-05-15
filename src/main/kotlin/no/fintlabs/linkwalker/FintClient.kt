67778be29840ae311052a3f047fe8e903159081a
Hknots <hknots99@gmail.com>
2025-05-15 14:25:31 +0200

    refactor: update FintClient to use webClient and change byte flow retrieval to JSON node

--- src/main/kotlin/no/fintlabs/linkwalker/FintClient.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/FintClient.kt
@@ -1,28 +1,27 @@
 package no.fintlabs.linkwalker
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.reactive.asFlow
-import org.springframework.core.io.buffer.DataBuffer
+import com.fasterxml.jackson.databind.JsonNode
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToFlux
 
 @Component
 class FintClient(
-    private val webclient: WebClient
+    private val webClient: WebClient
 ) {
 
-    fun getByteFlow(url: String, bearer: String?): Flow<ByteArray> =
-        webclient.get().uri(url)
-            .headers { h -> bearer?.let { h.setBearerAuth(it) } }
+    suspend fun getHateosResources(url: String, bearer: String): JsonNode =
+        webClient
+            .get()
+            .uri(url)
+            .headers { h -> h.setBearerAuth(bearer) }
+            .accept(MediaType.APPLICATION_JSON)
             .retrieve()
-            .bodyToFlux<DataBuffer>()
-            .map { db ->
-                val arr = ByteArray(db.readableByteCount())
-                db.read(arr)
-                db.release()
-                arr
-            }
-            .asFlow()
+            .bodyToMono(JsonNode::class.java)
+            .awaitSingle()
+
+    suspend fun getEmbeddedResources(url: String, bearer: String) =
+        getHateosResources(url, bearer).path("_embedded").path("_entries").toList()
 
 }
