1a22b86fcb7b550b43016f443ad958c8ed976904
Hknots <hknots99@gmail.com>
2025-05-15 14:25:13 +0200

    feat: add relation error cache configuration in CaffeineConfig

--- src/main/kotlin/no/fintlabs/linkwalker/config/CaffeineConfig.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/config/CaffeineConfig.kt
@@ -2,9 +2,11 @@ package no.fintlabs.linkwalker.config
 
 import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
+import no.fintlabs.linkwalker.RelationError
 import no.fintlabs.linkwalker.task.model.Task
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.Duration
 
 @Configuration
 class CaffeineConfig {
@@ -12,6 +14,13 @@ class CaffeineConfig {
     @Bean
     fun taskCache(): Cache<String, Task> =
         Caffeine.newBuilder()
+            .build()
+
+    @Bean
+    fun relationErrorCache(): Cache<String, MutableList<RelationError>> =
+        Caffeine.newBuilder()
+            .maximumSize(1_000)
+            .expireAfterWrite(Duration.ofHours(1))
             .build()
 
 }
