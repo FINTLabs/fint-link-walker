721ffeea83bfaa745873e239d3016af3d7fa04dc
Hknots <hknots99@gmail.com>
2025-05-15 14:26:16 +0200

    feat: extend Task data class to include relations, relationErrors, and status

--- src/main/kotlin/no/fintlabs/linkwalker/task/model/Task.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/task/model/Task.kt
@@ -1,9 +1,13 @@
 package no.fintlabs.linkwalker.task.model
 
+import no.fintlabs.linkwalker.model.Status
 import java.util.*
 
 data class Task(
-    val url: String
+    val url: String,
+    var relations: Int = 0,
+    var relationErrors: Int = 0,
+    var status: Status = Status.STARTED
 ) {
     val id = UUID.randomUUID().toString()
 }
