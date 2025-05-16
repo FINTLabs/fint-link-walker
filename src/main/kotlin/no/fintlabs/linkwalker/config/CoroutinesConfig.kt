a3aa930ff1eecae1ffe0c356d1188fd52d953693
Hknots <hknots99@gmail.com>
2025-05-16 09:06:08 +0200

    feat: add limited parallelism CoroutineDispatcher for task processing

--- src/main/kotlin/no/fintlabs/linkwalker/config/CoroutinesConfig.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/config/CoroutinesConfig.kt
@@ -1,7 +1,9 @@
 package no.fintlabs.linkwalker.config
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,5 +14,10 @@ class CoroutinesConfig {
     @Bean
     fun applicationScope(): CoroutineScope =
         CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
+    @Bean
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun taskProcessorDispatcher(): CoroutineDispatcher =
+        Dispatchers.IO.limitedParallelism(1)
 
 }
