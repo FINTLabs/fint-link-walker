51c6009ee9223913eefb4a6d00035403de61c964
Hknots <hknots99@gmail.com>
2025-05-16 08:22:07 +0200

    feat: implement retry mechanism for WebClient requests in FintClient

--- src/main/kotlin/no/fintlabs/linkwalker/FintClient.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/FintClient.kt
@@ -5,20 +5,33 @@ import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.util.retry.Retry
+import java.io.IOException
+import java.time.Duration
 
 @Component
 class FintClient(
     private val webClient: WebClient
 ) {
 
+    private companion object {
+        private val RETRY_SPEC = Retry
+            .backoff(3, Duration.ofMillis(250))
+            .filter { t ->
+                t is IOException ||
+                        (t is WebClientResponseException && t.statusCode.is5xxServerError)
+            }
+    }
+
     suspend fun getHateosResources(url: String, bearer: String): JsonNode =
-        webClient
-            .get()
+        webClient.get()
             .uri(url)
             .headers { h -> h.setBearerAuth(bearer) }
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
             .bodyToMono(JsonNode::class.java)
+            .retryWhen(RETRY_SPEC)
             .awaitSingle()
 
     suspend fun getEmbeddedResources(url: String, bearer: String) =
