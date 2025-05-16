034dd2ab119283430cf48ebdcd88a2a06cf8d4e5
Hknots <hknots99@gmail.com>
2025-05-16 13:34:10 +0200

    refactor: update RelationError data class to use relation and selfLink for improved clarity

--- src/main/kotlin/no/fintlabs/linkwalker/model/RelationError.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/model/RelationError.kt
@@ -1,7 +1,6 @@
 package no.fintlabs.linkwalker.model
 
 data class RelationError(
-    val baseUrl: String,
-    val count: Int,
-    val errors: Map<String, String>
+    val relation: String,
+    val selfLink: String
 )
