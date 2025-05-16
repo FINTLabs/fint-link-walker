1f78df79d76389ccfad60daae4ffcbc4ff7c89e8
Hknots <hknots99@gmail.com>
2025-05-16 16:19:15 +0200

    refactor: change relationErrors to AtomicInteger for thread-safe error counting

--- src/main/kotlin/no/fintlabs/linkwalker/task/model/Task.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/task/model/Task.kt
@@ -2,14 +2,15 @@ package no.fintlabs.linkwalker.task.model
 
 import no.fintlabs.linkwalker.model.Status
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
 data class Task(
     val url: String,
     val orgId: String,
     var relations: Int = 0,
-    var relationErrors: Int = 0,
+    var relationErrors: AtomicInteger = AtomicInteger(0),
     var status: Status = Status.IN_QUEUE
 ) {
     val id = UUID.randomUUID().toString()
-    val healthyRelations get() = relations - relationErrors
+    val healthyRelations get() = relations - relationErrors.get()
 }
