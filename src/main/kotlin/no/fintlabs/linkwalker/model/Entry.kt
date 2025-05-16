e73f1881c4beafcd3f016e0bdd1820a9ed7d647a
Hknots <hknots99@gmail.com>
2025-05-16 20:46:32 +0200

    feat: add malformedHrefs to Entry and unknownLinks to RelationReport for improved error tracking

--- src/main/kotlin/no/fintlabs/linkwalker/model/Entry.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/model/Entry.kt
@@ -2,7 +2,8 @@ package no.fintlabs.linkwalker.model
 
 data class Entry(
     val selfLink: String,
-    val ids: Map<String, Set<String>>
+    val ids: Map<String, Set<String>>,
+    val malformedHrefs: MutableList<String> = mutableListOf()
 ) {
 
     val idCount: Int
--- src/main/kotlin/no/fintlabs/linkwalker/model/RelationReport.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/model/RelationReport.kt
@@ -2,7 +2,8 @@ package no.fintlabs.linkwalker.model
 
 data class RelationReport(
     val baseUrl: String,
-    val relationErrors: MutableList<RelationError>
+    val relationErrors: MutableList<RelationError>,
+    val unknownLinks: MutableList<RelationError>
 ) {
     val errorCount get() = relationErrors.size
     val hasErrors get() = relationErrors.isNotEmpty()
