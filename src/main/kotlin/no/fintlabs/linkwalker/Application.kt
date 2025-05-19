475c0b079f996ac37de2d6f561ed213f99d68f8d
Hknots <hknots99@gmail.com>
2025-05-19 18:39:04 +0200

    feat: enable configuration properties scanning in the application

--- src/main/kotlin/no/fintlabs/linkwalker/Application.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/Application.kt
@@ -1,9 +1,11 @@
 package no.fintlabs.linkwalker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class FintCoreLinkWalkerApplication
 
 fun main(args: Array<String>) {
