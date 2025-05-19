0b212a9bf559f07a3a7e04dfcab8749be95a1ee6
Hknots <hknots99@gmail.com>
2025-05-19 19:17:37 +0200

    feat: add getTokenResponse method to AuthClient for token retrieval

--- src/main/kotlin/no/fintlabs/linkwalker/auth/AuthClient.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/auth/AuthClient.kt
@@ -1,7 +1,14 @@
 package no.fintlabs.linkwalker.auth
 
+import kotlinx.coroutines.reactive.awaitSingle
+import no.fintlabs.linkwalker.auth.model.AuthObject
+import no.fintlabs.linkwalker.auth.model.TokenResponse
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 
 @Component
@@ -10,6 +17,22 @@ class AuthClient(
     private val webClient: WebClient
 ) {
 
+    suspend fun getTokenResponse(authObject: AuthObject) =
+        webClient.post()
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(BodyInserters.fromFormData(createFormData(authObject)))
+            .retrieve()
+            .bodyToMono(TokenResponse::class.java)
+            .awaitSingle()
 
+    private fun createFormData(authObject: AuthObject): MultiValueMap<String, String> =
+        LinkedMultiValueMap<String, String>().apply {
+            add("grant_type", "password")
+            add("client_id", authObject.clientId)
+            add("client_secret", authObject.clientSecret)
+            add("username", authObject.name)
+            add("password", authObject.password)
+            add("scope", "fint-client")
+        }
 
 }
