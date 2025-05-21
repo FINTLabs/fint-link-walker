1de47dbcc9d47df744f64af6c41eb0f9a5e8746f
Hknots <hknots99@gmail.com>
2025-05-21 12:27:36 +0200

    fix: remove unnecessary @Configuration annotation from AuthProperties

--- src/main/kotlin/no/fintlabs/linkwalker/config/AuthProperties.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/config/AuthProperties.kt
@@ -1,9 +1,7 @@
 package no.fintlabs.linkwalker.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties("fint.linkwalker.auth")
 data class AuthProperties(
     val enabled: Boolean = false,
