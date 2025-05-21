1735e09297b20844193d85dd918ca52b821fd30e
Hknots <hknots99@gmail.com>
2025-05-21 12:27:52 +0200

    feat: add @JsonProperty annotations to TokenResponse fields for improved JSON serialization

--- src/main/kotlin/no/fintlabs/linkwalker/auth/model/TokenResponse.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/auth/model/TokenResponse.kt
@@ -1,8 +1,13 @@
 package no.fintlabs.linkwalker.auth.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class TokenResponse(
+    @JsonProperty("access_token")
     val accessToken: String,
+    @JsonProperty("token_type")
     val tokenType: String,
+    @JsonProperty("expires_in")
     val expiresIn: Int,
     val acr: String,
     val scope: String
