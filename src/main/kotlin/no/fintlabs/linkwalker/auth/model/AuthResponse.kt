dff48a3d495457740817226d009aa560be390c7a
Hknots <hknots99@gmail.com>
2026-02-11 14:10:56 +0100

    doc: add clarification on nullable AuthObject

--- src/main/kotlin/no/fintlabs/linkwalker/auth/model/AuthResponse.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/auth/model/AuthResponse.kt
@@ -3,6 +3,7 @@ package no.fintlabs.linkwalker.auth.model
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class AuthResponse(
+    // Auth object is only null if the client does not exist
     @get:JsonProperty("object")
     val authObject: AuthObject?,
     val orgId: String,
